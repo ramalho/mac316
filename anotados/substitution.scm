6c2fdeb3e27e55d6116ac3dc59056260c15875f8
Luciano Ramalho <luciano@ramalho.org>
2009-04-20 12:22:27 -0300

    os 3 exemplos de substituicao do cap. 3 com testes ilustrando as falhas

--- anotados/substitution.scm
+++ anotados/substitution.scm
@@ -21,20 +21,77 @@
 ; substitui sub-id por val em expr; a expressão resultante não tem
 ; instâncias livres de sub-id
 
+; No capítulo 3 de PLAI há três versões para a função de substituição,
+; aqui denominadas subst-v1, subst-v2 e subst-v3. A função é usada 
+; apenas no interpretador (calc) e em diretamente em alguns testes. 
+; Logo antes da definição de calc coloquei uma definição que associa 
+; subst a sobst-v1, subst-v2 ou subst-v3, para facilitar os testes com 
+; as três versões.
+
 ; PLAI section 3.1, p. 19-20
-(define (subst expr sub-id val)
+; subst renomeado para subst-v1
+; esta implementação gera exceção nos testes t5, t6, t10 e t11 abaixo
+(define (subst-v1 expr sub-id val)
   (type-case WAE expr
              [%num (n) expr]
-             [%add (l r) (%add (subst l sub-id val)
-                               (subst r sub-id val))]
-             [%sub (l r) (%sub (subst l sub-id val)
-                               (subst r sub-id val))]
+             [%add (l r) (%add (subst-v1 l sub-id val)
+                               (subst-v1 r sub-id val))]
+             [%sub (l r) (%sub (subst-v1 l sub-id val)
+                               (subst-v1 r sub-id val))]
              [%with (bound-id named-expr bound-body)
                     (if (symbol=? bound-id sub-id)
                         expr
                         (%with bound-id
                               named-expr
-                              (subst bound-body sub-id val)))]
+                              (subst-v1 bound-body sub-id val)))]
+
+             [%id (v) (if (symbol=? v sub-id) val expr)]
+  )
+)
+
+; PLAI section 3.3, p. 21-22 
+; subst renomeado para subst-v2
+; esta implementação gera exceção no teste t11
+(define (subst-v2 expr sub-id val)
+  (type-case WAE expr
+             [%num (n) expr]
+             [%add (l r) (%add (subst-v2 l sub-id val)
+                               (subst-v2 r sub-id val))]
+             [%sub (l r) (%sub (subst-v2 l sub-id val)
+                               (subst-v2 r sub-id val))]
+             [%with (bound-id named-expr bound-body)
+                    (if (symbol=? bound-id sub-id)
+                        expr
+                        (%with bound-id
+                               ; alteração: aplicar subst à expressão nomeada
+                               (subst-v2 named-expr sub-id val) 
+                               (subst-v2 bound-body sub-id val)))]
+
+             [%id (v) (if (symbol=? v sub-id) val expr)]
+  )
+)
+
+; PLAI section 3.3, p. 22 
+; subst renomeado para subst-v3
+; esta implementação passa em todos os testes de t1 a t11
+(define (subst-v3 expr sub-id val)
+  (type-case WAE expr
+             [%num (n) expr]
+             [%add (l r) (%add (subst-v3 l sub-id val)
+                               (subst-v3 r sub-id val))]
+             [%sub (l r) (%sub (subst-v3 l sub-id val)
+                               (subst-v3 r sub-id val))]
+             [%with (bound-id named-expr bound-body)
+                    (if (symbol=? bound-id sub-id)
+                        ; alteração: aplicar substituição à expressão nomeada
+                        ; mas não ao corpo, quando o identificador usado é
+                        ; o mesmo (veja exemplo no teste t11
+                        (%with bound-id
+                              (subst-v3 named-expr sub-id val)
+                              bound-body)
+                        (%with bound-id
+                               (subst-v3 named-expr sub-id val) 
+                               (subst-v3 bound-body sub-id val)))]
 
              [%id (v) (if (symbol=? v sub-id) val expr)]
   )
@@ -60,6 +117,13 @@
     ]
    )
   )
+
+
+;;; Definição da versão de substituição efetivamente usada no interpretador
+
+; (define subst subst-v1) ; gera exceção nos testes t5, t6, t10 e t11 abaixo
+; (define subst subst-v2) ; gera exceção no teste t11 abaixo
+(define subst subst-v3) ; passa em todos os testes de t1 a t11
 
 ;; calc : WAE!number
 ;; evaluates WAE expressions by reducing them to numbers
@@ -141,18 +205,21 @@
 ;;; Testes de parse e calc
 
 ; PLAI section 3.3, p. 21
-(test (calc (parse '5)) 5)
-(test (calc (parse '{+ 5 5})) 10)
-(test (calc (parse '{with {x {+ 5 5}} {+ x x}} ) ) 20)
-(test (calc (parse '{with {x 5} {+ x x}})) 10)
-;(test (calc (parse '{with {x {+ 5 5}} {with {y {- x 3}} {+ y y}}})) 14)
-;(test (calc (parse '{with {x 5} {with {y {- x 3}} {+ y y}}})) 4)
-(test (calc (parse '{with {x 5} {+ x {with {x 3} 10}}})) 15)
-(test (calc (parse '{with {x 5} {+ x {with {x 3} x}}})) 8)
-(test (calc (parse '{with {x 5} {+ x {with {y 3} x}}})) 10)
-;(test (calc (parse '{with {x 5} {with {y x} y}})) 5)
-;(test (calc (parse '{with {x 5} {with {x x} x}})) 5)
-
+(test (calc (parse '5)) 5) ; t1
+(test (calc (parse '{+ 5 5})) 10) ; t2
+(test (calc (parse '{with {x {+ 5 5}} {+ x x}} ) ) 20) ; t3
+(test (calc (parse '{with {x 5} {+ x x}})) 10) ; t4
+; t5: excecao "free identifier" com subst-v1
+(test (calc (parse '{with {x {+ 5 5}} {with {y {- x 3}} {+ y y}}})) 14) ; t5
+; t6: excecao "free identifier" com subst-v1
+(test (calc (parse '{with {x 5} {with {y {- x 3}} {+ y y}}})) 4) ; t6
+(test (calc (parse '{with {x 5} {+ x {with {x 3} 10}}})) 15) ; t7
+(test (calc (parse '{with {x 5} {+ x {with {x 3} x}}})) 8) ; t8
+(test (calc (parse '{with {x 5} {+ x {with {y 3} x}}})) 10) ; t9
+; t10: excecao "free identifier" com subst-v1
+(test (calc (parse '{with {x 5} {with {y x} y}})) 5) ; t10
+; t11: excecao "free identifier" com subst-v1 e subst-v2
+(test (calc (parse '{with {x 5} {with {x x} x}})) 5) ; t11
 
 ; exibir contagem de falhas, exceções e testes
 (define (contar-testes simbolo) 
