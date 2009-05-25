b4c1e4a533e6b0c64d5d24e29bfc49fbe6c65071
Luciano Ramalho <luciano@ramalho.org>
2009-05-25 19:40:08 -0300

    aula6 com prefixos #

---
+++ anotados/aula6-tests.scm
@@ -0,0 +1,69 @@
+;; The first three lines of this file were inserted by DrScheme. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "reader.ss" "plai" "lang")
+
+(include "aula6.scm")
+(print-only-errors #t)
+
+;; Testes do Prof. Gubi:
+
+; copiados de aula4a-tests.scm
+(test (tonum (interp (parse '(with (k (fun (x) (+ 3 x))) (+ 4 (call k 2)))))) 9)
+
+(test (tonum (interp (parse '{with {f {fun (x) (+ x 1)}} {f 2}}))) 3)
+(test (parse '{with {x 3}
+                {fun (y) {+ x y}}}) (%app (%fun 'x (%fun 'y (%add (%id 'x) (%id 'y)))) (%num 3)))
+
+
+(test (tonum 
+	   (interpd (parse 
+                     '(with (k (fun (x) (+ 3 x))) (+ 4 (call k 2))))  
+                    (mtSub)))
+      9)
+
+(test (parse 
+       '{with {x 3} {fun (y) {+ x y}}}) 
+      (%app (%fun 'x (%fun 'y (%add (%id 'x) (%id 'y)))) (%num 3)))
+
+
+(test (tonum (interp (parse '{with {f {fun (x) (+ x 1)}} {f 2}}))) 3)
+
+(test (parse '{with {x 3}
+                {fun (y) {+ x y}}}) 
+      (%app (%fun 'x (%fun 'y (%add (%id 'x) (%id 'y)))) (%num 3)))
+
+(test
+ (tonum
+   (interp 
+     (parse 
+      '(with 
+         (dq (with (d/dx (fun (f) 
+                         (fun (x) 
+                              (/(-(f (+ x 0.00001)) (f x)) 0.00001))))
+                (d/dx (fun (x) (* x x))))) 
+         (dq 9)))))
+  18.0)
+
+;; Nossos testes
+
+(test (tonum (interp (parse '{with {a 5}
+                                   {with {f {fun (x) (+ x a)}} {f 7}}}))) 12)
+
+;; O que está acontecendo aqui é o seguinte: quando o s1 é 
+;; aplicado em (s1 3) o símbolo "f" é encontrado na closure
+;; (veja este teste levantar uma exceção em aula4-tests.scm, pois
+;; lá a linguagem não implementa closures)
+
+(test
+ (interp 
+  (parse 
+   '(with 
+     (s1 (with (s2 (fun (f) 
+                        (fun (x) 
+                             (f (+ x 5)))))
+               (s2 (fun (x) (+ x 2))))) 
+     (s1 3))))
+ (numV 10))
+
+
+(include "test-summary.scm")
---
+++ anotados/aula6.scm
@@ -0,0 +1,105 @@
+
+; expressão aceita pela linguagem
+(define-type CFAE/L
+  [%num  (n number?)]
+  [%add  (lhs CFAE/L?) (rhs CFAE/L?)]
+  [%sub  (lhs CFAE/L?) (rhs CFAE/L?)]
+  [%mul  (lhs CFAE/L?) (rhs CFAE/L?)]
+  [%div  (lhs CFAE/L?) (rhs CFAE/L?)]
+  [%fun  (param symbol?)  (body CFAE/L?)]
+  [%id   (name symbol?)]
+  [%app  (fun-expr CFAE/L?) (arg-expr CFAE/L?)]
+  [%if0  (cond CFAE/L?) (e-yes CFAE/L?) (e-no CFAE/L?)] ;condicional simples 
+  )
+
+;; Tabela de simbolos
+(define-type Env
+  [mtSub]
+  [aSub (name symbol?) (value CFAE/L-Value?) (env Env?)]
+  )
+
+; busca linear
+(define (lookup name env)
+  (type-case Env env
+    [mtSub () (error 'lookup "no binding for identifier ~s" name)]
+    [aSub (bound-name bound-value rest-env)
+          (if (symbol=? bound-name name)
+              bound-value		; valor associado
+              (lookup name rest-env) ; procura no resto
+              )
+          ]
+    )
+  )
+
+
+;; Valor de retorno
+(define-type CFAE/L-Value
+  [numV (n number?)]
+  [closureV (param symbol?)
+            (body CFAE/L?)
+            (env Env?)]
+  [exprV (expr CFAE/L?)
+         (env Env?)]
+  )
+
+; interp : CFAE/L -> CFAE/L
+(define (interp expr) (interpd expr [mtSub]))
+
+; interpd : CFAE/L env -> CFAE/L
+(define (interpd expr env)
+  (type-case CFAE/L expr
+    [%num (n) (numV n)]
+    [%add (l r) (num+ (interpd l env) (interpd r env))]
+    [%sub (l r) (num- (interpd l env) (interpd r env))]
+    [%mul (l r) (num* (interpd l env) (interpd r env))]
+    [%div (l r) (num/ (interpd l env) (interpd r env))]
+    [%id (name) (lookup name env)]
+    [%fun (arg body) (closureV arg body env)]
+    [%app (fun-expr arg-expr)
+         (local ([define f (strict (interpd fun-expr env))]
+                 [define arg (exprV arg-expr env)]) ; troca pela expressão!
+           (interpd (closureV-body f) 
+                    (aSub (closureV-param f) 
+                          arg                   ;lazy!!!!!!
+                          (closureV-env f)))
+           )
+         ]
+    [%if0 (e1 ey en) 
+         (if (num-zero? (interpd e1 env)) 
+             (interpd ey env) ; sim
+             (interpd en env) ; não
+             )
+         ] 
+    ))
+
+(define (tonum nn) (numV-n nn))
+
+
+(define (num-op op l r) (numV (op (numV-n (strict l)) (numV-n (strict r)))))
+
+(define (num+ l r)  (num-op + l r))
+(define (num- l r)  (num-op - l r))
+(define (num* l r)  (num-op * l r))
+(define (num/ l r)  (num-op / l r))
+
+
+(define (num-zero? n) (zero? (numV-n (strict n))))
+
+;; strict CFAE/L-Value->CFAE/L-Value [sem exprV]
+(define (strict e)
+  (type-case CFAE/L-Value e
+    [exprV (expr env)
+           (local ([define val (strict (interpd expr env))])
+             (begin
+               (printf "Forçando exprV com ~a~n" val)
+               val))
+           ]
+    [else e]
+    ))
+
+
+; Rotina auxiliar
+
+(include "Parse-FAE.scm")
+
+;; Testes movidos para aula6-tests.scm
